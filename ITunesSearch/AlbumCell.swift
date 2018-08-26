25a604c300167e439bdf70286d1564c9dda64e0a
Roshy <Roshy_s7@icloud.com>
2018-08-26 17:51:21 +0300

    add Serch bar, pods, collection View
    Prepare for working , search state processing

---
+++ ITunesSearch/AlbumCell.swift
@@ -0,0 +1,17 @@
+//
+//  AlbumCell.swift
+//  ITunesSearch
+//
+//  Created by Admin on 26.08.2018.
+//  Copyright © 2018 Iulia Sigida. All rights reserved.
+//
+
+import UIKit
+
+class AlbumCell: UICollectionViewCell {
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var artistNameLabel: UILabel!
+    
+    
+}
---
+++ ITunesSearch/ITunesSearch/SearchResult.swift
@@ -0,0 +1,15 @@
+//
+//  SearchResult.swift
+//  SearchBarTable
+//
+//  Created by Admin on 25.08.2018.
+//  Copyright © 2018 Iulia Sigida. All rights reserved.
+//
+
+import Foundation
+class SearchResult {
+    var name = ""
+    var artistName = ""
+}
+    
+
