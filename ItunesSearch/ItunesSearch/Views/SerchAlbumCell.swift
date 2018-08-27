198e66babce8f5f1e9ab23af60ec842d1c8bf12f
Roshy <Roshy_s7@icloud.com>
2018-08-27 15:00:05 +0300

    Setup Views and UI

---
+++ ItunesSearch/ItunesSearch/Views/SerchAlbumCell.swift
@@ -0,0 +1,20 @@
+//
+//  SerchAlbumCell.swift
+//  ItunesSearch
+//
+//  Created by Admin on 26.08.2018.
+//  Copyright © 2018 Iulia Sigida. All rights reserved.
+//
+
+import UIKit
+
+class SerchAlbumCell: UICollectionViewCell {
+
+    @IBOutlet weak var artworkImageView: UIImageView!
+    @IBOutlet weak var albumNameLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
